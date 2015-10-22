add5de9d375dcde703f8e8ef314e038b81a2579a
Maximiliano Benedetto <infoprisma1@gmail.com>
2015-10-22 22:27:00 +0200

    change problem brute force

---
+++ change_problem_brute_force.js
@@ -0,0 +1,44 @@
+function changeProblemBruteForce(coins, amount, state) {
+	var nextIteration = 0,
+		defaultState = {quantity: Infinity, current: [] };
+
+	if(!coins.length) return copy(defaultState); 
+
+	//first run
+	if (!state) { 
+		state = copy(defaultState);
+	} else {
+
+		var bills = Math.floor(amount / coins[0]);
+		if (bills) {
+			state.quantity = state.quantity == Infinity ? bills : state.quantity + bills;
+			state.current.push({type: coins[0], quantity: bills})
+		}
+		amount -= bills * coins[0];
+		//we'll skip this coin in next iteration
+		nextIteration = 1;
+		
+	}
+
+	if (amount == 0) return copy(state);
+
+	var result1 = changeProblemBruteForce(coins.slice(nextIteration), amount, copy(state));
+	var result2 = changeProblemBruteForce(coins.slice(nextIteration+1), amount, copy(state));
+	
+	return result1.quantity > result2.quantity ? result2 : result1;
+	
+
+	function copy(data) {
+		return {
+			quantity: data.quantity,
+			current: data.current.slice(0)
+		}
+	}
+
+
+}
+
+
+var coins 	= [25,10,7,5,1],
+	amount 	= 105;
+console.log(changeProblemBruteForce(coins, amount));
